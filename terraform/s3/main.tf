0b969d20976dc8fd943e08983d747dee6f58b918
ArantesVini <vinicius.daniel02@gmail.com>
2023-07-10 09:39:24 -0300

    feat: add data and terraform script to s3 bucket

---
+++ terraform/s3/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "dsa-cloud-dw-20230710"
+
+  acl = "private"
+  tags = {
+    Name = "My Terraform Bucket"
+  }
+}
+
+resource "aws_s3_object" "my_folder" {
+  bucket = aws_s3_bucket.my_bucket.id
+  key    = "data/"
+}
+
+resource "aws_s3_object" "my_files" {
+  for_each = fileset("${path.cwd}/data", "**/*")
+
+  bucket  = aws_s3_bucket.my_bucket.id
+  key     = "data/${each.value}"
+  content = filebase64("${path.cwd}/data/${each.value}")
+}
